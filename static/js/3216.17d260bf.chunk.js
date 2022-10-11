f4b0f708e8a8a462b6f96e9ab2d3af5f4c0fe05b
Cande Aznarez <candeaznarez00@gmail.com>
2022-10-11 02:14:13 -0300

    Updates

---
+++ static/js/3216.17d260bf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadobe_template_generator=self.webpackChunkadobe_template_generator||[]).push([[3216],{3216:function(e,t,a){a.r(t);var i=new(a(8721).Z)({selectOption:{name:"Display name on list",value:"the name of this file",image:"",description:""},options:[{displayName:"Titulo",option:"title",isEditable:!0,isRequired:!0}],formTemplate:null,templateGenerator:function(e,t){return""}});t.default=i}}]);
+//# sourceMappingURL=3216.17d260bf.chunk.js.map
--- static/js/3216.b02ffbb0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadobe_template_generator=self.webpackChunkadobe_template_generator||[]).push([[3216],{3216:function(e,t,a){a.r(t);var i=new(a(8721).Z)({selectOption:{name:"Display name on list",value:"the name of this file",image:"",description:""},options:[{displayName:"Titulo",option:"title",isEditable:!0,isRequired:!0}],formTemplate:null,templateGenerator:function(e){return""}});t.default=i}}]);
-//# sourceMappingURL=3216.b02ffbb0.chunk.js.map
