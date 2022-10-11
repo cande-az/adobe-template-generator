3998ae246ab20a820784b8b6437446f830d62a41
Cande Aznarez <candeaznarez00@gmail.com>
2022-10-11 00:54:39 -0300

    Updates

--- src/templates/index.ts
+++
@@ -1,9 +0,0 @@
-// Import New Template
-import stepByStepTemplateData from "./instances/stepByStepTemplateData";
-import basicPopup from "./instances/basicPopup"
-
-const templates = (function () {
-  return [...[], stepByStepTemplateData.selectOption, basicPopup.selectOption];
-})();
-
-export { templates };
